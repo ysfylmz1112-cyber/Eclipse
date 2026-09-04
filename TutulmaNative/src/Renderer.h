9d3d202989e45e49cc6e8d12a957a192f4bdcf76
ysfylmz1112-cyber <ysfylmz1112@gmail.com>
2026-09-04 22:42:53 +0300

    Expose renderer vertex type for world geometry

--- TutulmaNative/src/Renderer.h
+++ TutulmaNative/src/Renderer.h
@@ -3,21 +3,12 @@
 #include <DirectXMath.h>
 #include <wrl/client.h>
 #include <vector>
-
-class Renderer final {
+class Renderer final{
 public:
-    bool Initialize(HWND window, int width, int height);
-    void Resize(int width, int height);
-    void BeginFrame(float eclipse);
-    void Draw(const DirectX::XMMATRIX& view, const DirectX::XMMATRIX& projection, float sunScale, float eclipse);
-    void EndFrame();
+ struct Vertex{DirectX::XMFLOAT3 position;DirectX::XMFLOAT3 normal;DirectX::XMFLOAT4 color;float emissive;};
+ struct ConstantBufferData{DirectX::XMMATRIX worldViewProjection;DirectX::XMFLOAT4 tint;};
+ bool Initialize(HWND,int,int);void Resize(int,int);void BeginFrame(float);void Draw(const DirectX::XMMATRIX&,const DirectX::XMMATRIX&,float,float);void EndFrame();
 private:
-    struct Vertex { DirectX::XMFLOAT3 position; DirectX::XMFLOAT3 normal; DirectX::XMFLOAT4 color; float emissive; };
-    struct ConstantBufferData { DirectX::XMMATRIX worldViewProjection; DirectX::XMFLOAT4 tint; };
-    bool CreateDevice(HWND window); bool CreateShaders(); bool CreateGeometry(); bool CreateTargets(int width,int height); bool CreateRasterizerState();
-    Microsoft::WRL::ComPtr<ID3D11Device> device_; Microsoft::WRL::ComPtr<ID3D11DeviceContext> context_; Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain_;
-    Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTarget_; Microsoft::WRL::ComPtr<ID3D11DepthStencilView> depthView_;
-    Microsoft::WRL::ComPtr<ID3D11VertexShader> vertexShader_; Microsoft::WRL::ComPtr<ID3D11PixelShader> pixelShader_; Microsoft::WRL::ComPtr<ID3D11InputLayout> inputLayout_;
-    Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer_; Microsoft::WRL::ComPtr<ID3D11Buffer> constantBuffer_; Microsoft::WRL::ComPtr<ID3D11RasterizerState> rasterizerState_;
-    UINT vertexCount_=0; int width_=1; int height_=1;
+ bool CreateDevice(HWND);bool CreateShaders();bool CreateGeometry();bool CreateTargets(int,int);bool CreateRasterizerState();
+ Microsoft::WRL::ComPtr<ID3D11Device> device_;Microsoft::WRL::ComPtr<ID3D11DeviceContext> context_;Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain_;Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTarget_;Microsoft::WRL::ComPtr<ID3D11DepthStencilView> depthView_;Microsoft::WRL::ComPtr<ID3D11VertexShader> vertexShader_;Microsoft::WRL::ComPtr<ID3D11PixelShader> pixelShader_;Microsoft::WRL::ComPtr<ID3D11InputLayout> inputLayout_;Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer_;Microsoft::WRL::ComPtr<ID3D11Buffer> constantBuffer_;Microsoft::WRL::ComPtr<ID3D11RasterizerState> rasterizerState_;UINT vertexCount_=0;int width_=1,height_=1;
 };
