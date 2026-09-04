e75c196bf191343345b2c7ce8199de1d4b522aca
ysfylmz1112-cyber <ysfylmz1112@gmail.com>
2026-09-04 21:48:01 +0300

    Make player controller input-system independent

--- Tutulma/UnityProject/Assets/Scripts/Player/PlayerController.cs
+++ Tutulma/UnityProject/Assets/Scripts/Player/PlayerController.cs
@@ -5,8 +5,10 @@ namespace Tutulma.Player
     [RequireComponent(typeof(CharacterController))]
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private float moveSpeed = 5f;
-        [SerializeField] private float gravity = -20f;
+        [SerializeField] private float moveSpeed = 6f;
+        [SerializeField] private float sprintSpeed = 10f;
+        [SerializeField] private float gravity = -25f;
+        [SerializeField] private float jumpHeight = 1.4f;
 
         private CharacterController controller;
         private Vector3 velocity;
@@ -18,20 +20,29 @@ namespace Tutulma.Player
 
         private void Update()
         {
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
+            float x = 0f;
+            float z = 0f;
 
-            Vector3 input = new Vector3(horizontal, 0f, vertical);
-            input = Vector3.ClampMagnitude(input, 1f);
+            if (Input.GetKey(KeyCode.A)) x -= 1f;
+            if (Input.GetKey(KeyCode.D)) x += 1f;
+            if (Input.GetKey(KeyCode.S)) z -= 1f;
+            if (Input.GetKey(KeyCode.W)) z += 1f;
 
-            Vector3 movement = transform.TransformDirection(input) * moveSpeed;
-            controller.Move(movement * Time.deltaTime);
+            Vector3 input = Vector3.ClampMagnitude(new Vector3(x, 0f, z), 1f);
+            float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
+            Vector3 movement = transform.TransformDirection(input) * speed;
 
-            if (controller.isGrounded && velocity.y < 0f)
-                velocity.y = -2f;
+            if (controller.isGrounded)
+            {
+                if (velocity.y < 0f)
+                    velocity.y = -2f;
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
 
             velocity.y += gravity * Time.deltaTime;
-            controller.Move(velocity * Time.deltaTime);
+            controller.Move((movement + velocity) * Time.deltaTime);
         }
     }
 }
