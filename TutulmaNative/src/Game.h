d4829b4c8a2093d9d212406e728d1de188b0576e
ysfylmz1112-cyber <ysfylmz1112@gmail.com>
2026-09-04 22:43:21 +0300

    Add eclipse progression state to game

--- TutulmaNative/src/Game.h
+++ TutulmaNative/src/Game.h
@@ -1,21 +1,9 @@
 #pragma once
-
 #include "Camera.h"
 #include "Renderer.h"
-
-class Game final {
+class Game final{
 public:
-    bool Initialize(HWND window, int width, int height);
-    void Resize(int width, int height);
-    void Update(float dt);
-    void Render();
-    void OnMouseDelta(float dx, float dy);
-    void OnKeyDown(WPARAM key);
-    void OnKeyUp(WPARAM key);
-
+ bool Initialize(HWND,int,int);void Resize(int,int);void Update(float);void Render();void OnMouseDelta(float,float);void OnKeyDown(WPARAM);void OnKeyUp(WPARAM);
 private:
-    Renderer renderer_;
-    Camera camera_;
-    bool keys_[256]{};
-    float anomaly_ = 1.0f;
+ Renderer renderer_;Camera camera_;bool keys_[256]{};float anomaly_=0;float eclipse_=0;
 };
